7f8c8ea2ac4582a4ac8e43ba71b31f577dc5c404
Michael Sunday <olawuni.michael@gmail.com>
2017-12-21 12:24:33 +0100

    First Commit

---
+++ js/recursive.js
@@ -0,0 +1,13 @@
+<html>
+	<head><title>Recursive Function</title></head>
+	<body>
+		<script type="text/javascript">
+			function countToHundred(){
+				for(i=1; i <=100; i++){
+					console.log("Number " + i);
+				}
+				countToHundred();
+			}
+		</script>
+	</body>
+</html>
